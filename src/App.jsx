0d5719eb038d73509611a3f539b2b9964b258004
Mark Howe <mvahowe@gmail.com>
2025-04-21 11:01:24 +0200

    Multiple sources, snackbar, refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,61 @@
 import {useState, useEffect} from "react"
-import {Grid2} from "@mui/material";
+import {Button, ButtonGroup, Grid2} from "@mui/material";
 import {CloudDownload, CloudDone} from "@mui/icons-material";
+import {enqueueSnackbar} from "notistack";
 import {getAndSetJson, getJson} from "pithekos-lib";
 
 function App() {
+    const sourceWhitelist = [
+        ["git.door43.org/BurritoTruck", "Burrito Truck (Door43)"],
+        ["git.door43.org/uW", "unfoldingWord Burritos (Door43)"],
+    ];
+    const [remoteSource, setRemoteSource] = useState(sourceWhitelist[0]);
+    console.log(remoteSource);
     const [catalog, setCatalog] = useState([]);
     useEffect(
-        () => {getAndSetJson({
-                url: "/gitea/remote-repos/git.door43.org/BurritoTruck",
+        () => {
+            getAndSetJson({
+                url: `/gitea/remote-repos/${remoteSource[0]}`,
                 setter: setCatalog
-            }).then()},
-        []
+            }).then()
+        },
+        [remoteSource]
     );
 
     const [localRepos, setLocalRepos] = useState([]);
     useEffect(
-        () => {getAndSetJson({
+        () => {
+            getAndSetJson({
                 url: "/git/list-local-repos",
                 setter: setLocalRepos
-            }).then()},
-        []
+            }).then()
+        },
+        [remoteSource]
     );
 
     return (
         <Grid2 container spacing={2}>
             <Grid2 container>
+                <Grid2 item size={12}>
+                    <ButtonGroup>
+                        {
+                            sourceWhitelist.map(
+                                s => <Button
+                                    variant={s[0] === remoteSource[0] ? "contained" : "outlined"}
+                                    onClick={() => setRemoteSource(s)}
+                                >
+                                    {s[1]}
+                                </Button>
+                            )
+                        }
+                    </ButtonGroup>
+                </Grid2>
                 {
                     catalog
                         .filter(ce => ce.flavor)
                         .map(
                             ce => {
-                                const remoteRepoPath = `git.door43.org/BurritoTruck/${ce.name}`;
+                                const remoteRepoPath = `${remoteSource[0]}/${ce.name}`;
                                 return <>
                                     <Grid2 item size={1}>
                                         {ce.abbreviation.toUpperCase()}
@@ -51,9 +76,25 @@ function App() {
                                                 <CloudDownload
                                                     disabled={localRepos.includes(remoteRepoPath)}
                                                     onClick={async () => {
-                                                        await getJson(`/git/fetch-repo/${remoteRepoPath}`);
-                                                        window.location.href = "/";
-                                                    }}
+                                                        enqueueSnackbar(
+                                                            `Downloading ${ce.abbreviation}`,
+                                                            {variant: "info"}
+                                                        );
+                                                        const fetchResponse = await getJson(`/git/fetch-repo/${remoteRepoPath}`);
+                                                        if (fetchResponse.ok) {
+                                                            enqueueSnackbar(
+                                                                `${ce.abbreviation} downloaded`,
+                                                                {variant: "success"}
+                                                            );
+                                                            setRemoteSource([...remoteSource]) // Trigger local repo check
+                                                        } else {
+                                                            enqueueSnackbar(
+                                                                `${ce.abbreviation} failed`,
+                                                                {variant: "error"}
+                                                            );
+                                                        }
+                                                    }
+                                                    }
                                                 />
                                         }
                                     </Grid2>
